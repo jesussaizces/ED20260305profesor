07478abf42cd216536c17cfa25152217449bdc9c
jesus.saices <jesus.saices@ED09012096P402.educa.jcyl.es>
2026-03-12 10:10:36 +0100

    Cambios del LanzadorTablaMultiplicar

--- src/jcolonia/daw2025/tablasmvc/LanzadorTablaMultiplicar.java
+++ src/jcolonia/daw2025/tablasmvc/LanzadorTablaMultiplicar.java
@@ -1,18 +1,19 @@
 package jcolonia.daw2025.tablasmvc;
+
 /**
-* Lanzador de aplicación de consola de texto con menús. Aplicación
-* de texto usando tablas de multiplicar infantiles clásicas. 
-*/
+ * Lanzador de aplicación de consola de texto con menús. Aplicación
+ * de texto usando tablas de multiplicar infantiles clásicas. 
+ */
 public class LanzadorTablaMultiplicar {
 	/**
-	* Inicia el menú principal del programa.
-	* @param argumentos opciones de ejecución -no se usan-
-	*/
+	 * Inicia el menú principal del programa.
+	 * @param argumentos opciones de ejecución -no se usan-
+	 */
 	public static void main (String[] argumentos){
 		ControlTablaMultiplicar programa;
 		
-		programa = new ControlTablaMultiplicar()
+		programa = new ControlTablaMultiplicar();
 		
-		programa.bucleprincipal();
+		programa.buclePrincipal();
 	}
 }
